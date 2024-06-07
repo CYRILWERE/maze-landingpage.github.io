1a03a8c60c44ac8e581a0ffa93d668402eda2731
CYRILWERE <werecyril20@gmail.com>
2024-06-07 22:52:00 +0200

    landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+function startGame(level) {
+    alert(`Starting game at ${level} level!`);
+    // Here, you would redirect to the actual game page or initialize the game with the selected difficulty level.
+    // For example, you could use window.location.href = `game.html?level=${level}`;
+    // Or initialize the game
+
